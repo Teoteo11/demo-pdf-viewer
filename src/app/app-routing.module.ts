e36558fa6221407c36436f53821ca4d846f92c35
Teoteo11 <depaym69@gmail.com>
2023-02-01 23:16:24 +0100

    add a few style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomPdfViewerComponent } from './components/custom-pdf-viewer/custom-pdf-viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pdf-viewer-one', component: CustomPdfViewerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PDFProgressData } from 'ng2-pdf-viewer';
 import { delay, map, of } from 'rxjs';
 
@@ -8,6 +9,14 @@ import { delay, map, of } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router: Router) {
+
+  }
+
   title = 'demo-pdf-viewer';
+
+  openPdfViewer = (value: 1 | 2) => this.router.navigate([`pdf-viewer-${value === 1 ? 'one' : 'two'}`]); 
+  
 
 }
